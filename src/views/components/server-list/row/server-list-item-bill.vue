3501483af0988a0cbe618bbf491c95224df634a0
hi2hi <code@hi2hi.cc>
2024-12-14 06:28:54 +0000

    ✨ 新增行列的单独组件

--- src/views/components/server-list/row/server-list-item-bill.vue
+++ src/views/components/server-list/row/server-list-item-bill.vue
@@ -1,49 +1,32 @@
 <template>
-  <div
+  <server-list-column
     v-if="extraFields?.remainingTime"
-    class="list-column-item list-column-item--remaining-time"
-  >
-    <div class="list-column-item-content">
-      <span class="item-label">剩余</span>
-      <div class="item-content">
-        <span class="text-item value-text">{{ billAndPlan?.remainingTime?.value || '-' }}</span>
-      </div>
-    </div>
-  </div>
-  <div
+    prop="remaining-time"
+    label="剩余"
+    :value="billAndPlan?.remainingTime?.value || '-'"
+  />
+  <server-list-column
     v-if="extraFields?.billing"
-    class="list-column-item list-column-item--billing"
-  >
-    <div class="list-column-item-content">
-      <span
-        v-if="!billAndPlan?.billing?.isFree && billAndPlan?.billing?.label"
-        class="item-label"
-      >
-        {{ billAndPlan.billing.label }}
-      </span>
-      <div class="item-content">
-        <span class="text-item value-text">{{ billAndPlan?.billing?.value || '-' }}</span>
-      </div>
-    </div>
-  </div>
-  <div
+    prop="billing"
+    label="费用"
+    :value="billAndPlan?.billing?.value || '-'"
+  />
+  <server-list-column
     v-if="extraFields?.orderLink"
-    class="list-column-item list-column-item--order-link"
+    prop="order-link"
+    label="链接"
+    wdith="80"
+    :slot-content="true"
   >
-    <div class="list-column-item-content">
-      <span class="item-label">链接</span>
-      <div class="item-content">
-        <span
-          v-if="showBuyBtn"
-          class="text-item value-text"
-          @click="toBuy"
-        >
-          {{ buyBtnText }}
-        </span>
-        <span v-else>-</span>
-      </div>
-    </div>
-  </div>
+    <span
+      v-if="showBuyBtn"
+      class="order-link"
+      @click="toBuy"
+    >
+      {{ buyBtnText }}
+    </span>
+    <span v-else>-</span>
+  </server-list-column>
 </template>
 
 <script setup>
@@ -58,6 +41,8 @@ import {
 import config from '@/config';
 
 import handleServerBillAndPlan from '@/views/composable/server-bill-and-plan';
+
+import ServerListColumn from './server-list-column.vue';
 
 const props = defineProps({
   info: {
@@ -81,60 +66,15 @@ const {
 const buyBtnText = computed(() => config.nazhua.buyBtnText || '购买');
 const showBuyBtn = computed(() => !!props.info?.PublicNote?.customData?.orderLink);
 
-// function toBuy() {
-//   const decodeUrl = decodeURIComponent(props.info?.PublicNote?.customData?.orderLink);
-//   window.open(decodeUrl, '_blank');
-// }
+function toBuy() {
+  const decodeUrl = decodeURIComponent(props.info?.PublicNote?.customData?.orderLink);
+  window.open(decodeUrl, '_blank');
+}
 </script>
 
 <style lang="scss" scoped>
-.list-column-item {
-
-  .list-column-item-content {
-    --item-content-label-height: 16px;
-    --item-content-value-height: 24px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: var(--list-item-height);
-
-    .item-label {
-      padding-top: 6px; // 视觉修正
-      line-height: var(--item-content-label-height);
-      font-size: 12px;
-      color: #ccc;
-    }
-
-    .item-content {
-      line-height: var(--item-content-value-height);
-      font-size: 14px;
-    }
-  }
-
-  &--remaining-time {
-    width: 60px;
-
-    .value-text {
-      color: #74dbef;
-    }
-  }
-
-  &--billing {
-    width: 60px;
-
-    .value-text {
-      color: var(--list-item-price-color);
-    }
-  }
-
-  &--order-link {
-    width: 60px;
-
-    .value-text {
-      color: var(--list-item-buy-link-color);
-      cursor: pointer;
-    }
-  }
+.order-link {
+  color: var(--list-item-buy-link-color);
+  cursor: pointer;
 }
 </style>
--- src/views/components/server-list/row/server-list-item-real-time.vue
+++ src/views/components/server-list/row/server-list-item-real-time.vue
@@ -1,18 +1,12 @@
 <template>
-  <div
+  <server-list-column
     v-for="item in serverRealTimeList"
     :key="item.key"
-    class="list-column-item list-column-item--real-time"
-    :class="`list-column-item--real-time-${item.key}`"
-  >
-    <div class="real-time-content">
-      <span class="item-label">{{ item.label }}</span>
-      <div class="item-content">
-        <span class="item-value">{{ item.show ? item?.value : '-' }}</span>
-        <span class="item-unit item-text">{{ item.show ? item?.unit : '' }}</span>
-      </div>
-    </div>
-  </div>
+    :prop="item.key"
+    :label="item.label"
+    :value="item.show ? item?.value : '-'"
+    :unit="item.show ? item?.unit : ''"
+  />
 </template>
 
 <script setup>
@@ -23,6 +17,8 @@ import {
   inject,
 } from 'vue';
 import handleServerRealTime from '@/views/composable/server-real-time';
+
+import ServerListColumn from './server-list-column.vue';
 
 const props = defineProps({
   info: {
@@ -47,59 +43,3 @@ const {
   serverRealTimeListTpls: props.serverRealTimeListTpls,
 });
 </script>
-
-<style lang="scss" scoped>
-.list-column-item {
-  .real-time-content {
-    --real-time-label-height: 16px;
-    --real-time-value-height: 24px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: var(--list-item-height);
-
-    .item-label {
-      padding-top: 6px; // 视觉修正
-      line-height: var(--real-time-label-height);
-      font-size: 12px;
-      color: #ccc;
-    }
-    .item-content {
-      line-height: var(--real-time-value-height);
-      font-size: 14px;
-    }
-  }
-
-  &--real-time-duration {
-    width: 50px;
-    .item-value {
-      color: var(--duration-color);
-    }
-  }
-  &--real-time-load {
-    width: 50px;
-    .item-value {
-      color: var(--load-color);
-    }
-  }
-  &--real-time-transfer {
-    width: 80px;
-    .item-value {
-      color: var(--transfer-color);
-    }
-  }
-  &--real-time-inSpeed {
-    width: 50px;
-    .item-value {
-      color: var(--net-speed-in-color);
-    }
-  }
-  &--real-time-outSpeed {
-    width: 50px;
-    .item-value {
-      color: var(--net-speed-out-color);
-    }
-  }
-}
-</style>
